e3557dc31ddf677d065728be60b5ca8a7d4a163b
Maximiliano Sosa <msosa_@hotmail.com>
2021-10-07 10:39:07 -0300

    Add project files.

---
+++ C2D2/Controllers/TestController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace C2D2.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+
+        [HttpGet("traerfechaactual")]
+        public DateTime TraerFechaActual()
+        {
+            return DateTime.Now;
+        }
+
+        [HttpGet("traerhoraactual")]
+        public string TraerHoraActual()
+        {
+            return DateTime.Now.ToShortTimeString();
+        }
+    }
+}
